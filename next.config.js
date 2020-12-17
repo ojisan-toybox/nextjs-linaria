639221fbdab8e22ad15b81f642f92b1554c85beb
sadness_ojisan <sadness.ojisan@gmail.com>
2020-12-18 01:55:31 +0900

    fix config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const withLinaria = require("next-linaria");
+require("dotenv").config();
+
+const repoName = "/nextjs-linaria";
 module.exports = withLinaria({
   linaria: {
     /* linaria options here */
   },
+  assetPrefix: process.env.GITHUB_PAGES ? repoName : "",
 });
